ca2a2094b4e8f8257410ea72f4d5db9126e239b7
Abhay Buch <abhay@makenotion.com>
2023-10-16 22:07:07 -0700

    update binding

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const active_app = require('./build/Release/active_app');
+const active_app = require('bindings')('active_app.node');
 
-module.exports = active_app.makeKeyAndOrderFront
+module.exports = {
+	makeKeyAndOrderFront: active_app.makeKeyAndOrderFront
+}
